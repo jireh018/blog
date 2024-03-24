43fd8263244cb94fa62759e66b9adeb1fd1d244c
jireh018 <jirehtuto71@gmail.com>
2024-03-24 17:37:11 -0400

    express async error added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("express-async-errors");
 const express = require("express");
 const app = express();
 //router
 const auth = require("./routes/auth");
 //db
 const mongoose = require("mongoose");
+//middleware
 
 app.use(express.json());
 app.use("/api/auth", auth);
