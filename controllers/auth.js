8c05a04e84e539cf80d6a7a02474bb1553dc322f
jireh018 <jirehtuto71@gmail.com>
2024-03-24 19:05:12 -0400

    bcrypt - hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
 const { User, validateUser } = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
   const { error } = validateUser(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const user = new User(req.body);
-  console.log(user, req.body);
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
   res.status(201).json(user);
 };
